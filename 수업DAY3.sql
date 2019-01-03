eeebb0f3a7387c825c72b33ee95e5cf59315071e
Seokho Youn <axlover@hotmail.com>
2019-01-03 21:28:19 +0900

    Update 수업DAY3.sql

--- 수업DAY3.sql
+++ 수업DAY3.sql
@@ -370,28 +370,36 @@ FROM EMPLOYEE;
 --  단, 주민번호 9번째 자리부터 끝까지는 '*'문자로 채움
 --  예 : 홍길동 771120-1******
 
-SELECT 
+SELECT EMP_NAME 직원명, RPAD(SUBSTR(EMP_NO,1,8),14,'*') 주민번호
+FROM EMPLOYEE;
 
 --2. 직원명, 직급코드, 연봉(원) 조회
 --  단, 연봉은 ￦57,000,000 으로 표시되게 함
 --     연봉은 보너스포인트가 적용된 1년치 급여임
---
---
+
+SELECT EMP_NAME 직원명, JOB_ID 직급코드, TO_CHAR((SALARY+(NVL(BONUS_PCT,0)*SALARY)*12), 'L999,999,999') "연봉(원)"
+FROM EMPLOYEE
+ORDER BY "연봉(원)" DESC;
+
 --3. 부서코드가 50, 90인 직원들 중에서 2004년도에 입사한 직원의 
 --   수 조회함.
 --	사번 사원명 부서코드 입사일
---
---
+SELECT COUNT(*) AS  "04년도에 입사한직원수"
+FROM EMPLOYEE
+WHERE (DEPT_ID = '50' OR DEPT_ID = '90') AND TO_CHAR(HIRE_DATE, 'RRRR') = 2004;
+
 --4. 직원명, 입사일, 입사한 달의 근무일수 조회
 --  단, 주말도 포함함
---
---
+SELECT EMP_NAME, HIRE_DATE, LAST_DAY(HIRE_DATE) - HIRE_DATE
+FROM EMPLOYEE;
+
 --5. 직원명, 부서코드, 생년월일, 나이(만) 조회
 --  단, 생년월일은 주민번호에서 추출해서, 
 --     ㅇㅇ년 ㅇㅇ월 ㅇㅇ일로 출력되게 함.
 --  나이는 주민번호에서 추출해서 날짜데이터로 변환한 다음, 계산함
---
---
+
+SELECT EMP_NAME, DEPT_ID, TO_CHAR(TO_DATE(SUBSTR,1,6))
+
 --6. 직원들의 입사일로 부터 년도만 가지고, 각 년도별 입사인원수를 구하시오.
 --  아래의 년도에 입사한 인원수를 조회하시오.
 --  => to_char, decode, sum 사용
